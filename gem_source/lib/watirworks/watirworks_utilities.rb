efa440e2b5a80d2a5c00ee109b5001cc4dd27de5
Joseph DiMauro <joseph.dimauro@echostar.com>
2016-04-01 08:52:51 -0600

    Built gem. Turned off $VERBOSE flag in parse_spreadsheet()

--- gem_source/lib/watirworks/watirworks_utilities.rb
+++ gem_source/lib/watirworks/watirworks_utilities.rb
@@ -3049,14 +3049,14 @@ module WatirWorks_Utilities
 
       aSheetsInWorkbook = oWorkbook.sheets
 
-      #if($VERBOSE == true)
-      puts2("\t Workbook contains spreadsheets: " )
+      if($VERBOSE == true)
+        puts2("\t Workbook contains spreadsheets: " )
 
-      # Loop through sheets in workbook
-      aSheetsInWorkbook.each do | sSheetLabel |
-        puts2("\t\t" + sSheetLabel)
-      end # Loop through sheets in workbook
-      #end
+        # Loop through sheets in workbook
+        aSheetsInWorkbook.each do | sSheetLabel |
+          puts2("\t\t" + sSheetLabel)
+        end # Loop through sheets in workbook
+      end
 
       # Set the specified Spreadsheet as the default (active) sheet
       if(sSpreadsheet == nil)
